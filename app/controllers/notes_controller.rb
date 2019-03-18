7bcc2f3a8f4646b1739478923afd8b6dff3c0b0b
EC2 Default User <bgueterman@gmail.com>
2019-03-18 00:11:35 +0000

    Touched up page. Static Header. Create functionality. Footer. Tweaked layout some.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,19 @@
 class NotesController < ApplicationController
   def index
+    @note = Note.all
+  end
+  
+  def new
+  end
+  
+  def create
+    @note = Note.new(note_params)
+    @note.save
+    redirect_to action: "index"
   end
 end
+
+private
+    def note_params
+        params.require(:notes).permit(:text)
+    end
