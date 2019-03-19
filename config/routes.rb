434d4733dd31fa6998da3f77e77cee41733401f6
EC2 Default User <bgueterman@gmail.com>
2019-03-19 22:11:05 +0000

    Edit functional, reworked note layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     get 'notes/index'
-    
+
     resources :notes
     root 'notes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
